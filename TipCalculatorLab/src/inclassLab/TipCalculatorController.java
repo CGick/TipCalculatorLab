0757d0860645e4ea38dca3d064c69e42445eb113
SK207_29 <SK207_29@K207_W829.carrollcc.edu>
2016-03-30 20:24:34 -0400

    update

--- TipCalculatorLab/src/inclassLab/TipCalculatorController.java
+++ TipCalculatorLab/src/inclassLab/TipCalculatorController.java
@@ -2,6 +2,11 @@ package inclassLab;
 
 import java.text.DecimalFormat;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class TipCalculatorController {
 
 	DecimalFormat money = new DecimalFormat("$#,##0.00");
@@ -15,7 +20,13 @@ public class TipCalculatorController {
     @FXML
     private Label lblTotal;
 
+    private double amount=0, tax=0, tip=0;
+    
     @FXML
-    void calculateTip(ActionEvent event) {
+    void calculateTip() {
+    	amount = Double.parseDouble(tfBill.toString());
+    	tax = (amount * .06);
+    	tip = ((amount + tax) *.15);
+    }
 
 }
